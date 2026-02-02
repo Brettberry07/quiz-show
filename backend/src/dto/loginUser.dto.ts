fe7128ee41b6b9f7db7d324dfa279da3a2df9dbc
Cameron Ginther <cameronginther@gmail.com>
2026-02-02 09:46:40 -0600

    Removed redundant character variable

--- backend/src/dto/loginUser.dto.ts
+++ backend/src/dto/loginUser.dto.ts
@@ -1,10 +1,8 @@
 import { IsString, IsNotEmpty, MaxLength } from "class-validator";
 
-export const USERNAME_MAX_LENGTH = 20;
-
 export class LoginUserDto {
   @IsString({ message: "Username must be a string" })
   @IsNotEmpty({ message: "Username is required" })
-  @MaxLength(USERNAME_MAX_LENGTH, { message: `Username must be at most ${USERNAME_MAX_LENGTH} characters` })
+  @MaxLength(20, { message: `Username must be at most 20 characters` })
   username: string;
 }
